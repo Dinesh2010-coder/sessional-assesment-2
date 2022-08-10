3d029f660fa7c03ce0b9bc94fa0eb3d4a0c96895
Dinesh2010-coder <dinesh0206.be20@chitkara.edu.in>
2022-08-11 00:32:17 +0530

    Blog added

--- script.js
+++ script.js
@@ -90,3 +90,37 @@ function validateForm(){
         return false;
     }
 }
+
+function btnClickHandeler() {
+    var xhr = new XMLHttpRequest()
+    console.log(xhr);
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = this.responseText;
+            console.log(this.responseText);
+            var dataObject = JSON.parse(data)
+            for (var i = 0; i < dataObject.length; i++) {
+                var tr = document.createElement("tr")
+                var td1 = document.createElement("td")
+                var td2 = document.createElement("td")
+                var td3 = document.createElement("td")
+                var td4 = document.createElement("td");
+                var td5 = document.createElement("td");
+                td1.innerHTML = dataObject[i].name;
+                td2.innerHTML = dataObject[i].phone;
+                td3.innerHTML = dataObject[i].email;
+                td4.innerHTML = dataObject[i].username;
+                td5.innerHTML = dataObject[i].address.city;
+                tr.appendChild(td1)
+                tr.appendChild(td2)
+                tr.appendChild(td3)
+                tr.appendChild(td4)
+                tr.appendChild(td5)
+                document.getElementById("test").appendChild(tr);
+            }
+
+        }
+    }
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09", true);
+    xhr.send();
+}
